68766e825df850eab5431f416b50c595606f8958
wenke.lai <wenke.studio@gmail.com>
2024-02-25 21:56:07 +0800

    fix(aws.ec2): add public-key prop to KeyPair

--- integrator/aws/ec2/key_pair.py
+++ integrator/aws/ec2/key_pair.py
@@ -1,12 +1,15 @@
+from pathlib import Path
+
 from pulumi_aws import ec2
 
 
 class KeyPair(ec2.KeyPair):
-    def __init__(self, name: str, **kwargs) -> None:
+    def __init__(self, name: str, public_key: Path | str, **kwargs) -> None:
         """Create a new EC2 Key Pair.
 
         Args:
             name (str): The name of the EC2 Key Pair.
             **kwargs: [additional arguments](https://www.pulumi.com/registry/packages/aws/api-docs/ec2/keypair/#inputs)
         """
-        super().__init__(name, **kwargs)
+        with open(public_key, "r", encoding="utf-8") as fr:
+            super().__init__(name, public_key=fr.read(), **kwargs)
