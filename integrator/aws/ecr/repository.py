df07a4b16b04c9f1a5d664af34a8ee63d23448ea
wenke.lai <wenke.studio@gmail.com>
2024-03-01 16:37:53 +0800

    fix(aws.ecr): fix the conflict between name and resource-name

--- integrator/aws/ecr/repository.py
+++ integrator/aws/ecr/repository.py
@@ -1,15 +1,25 @@
+from typing import Optional
+
 from pulumi_aws import ecr
 
 from diagrams.eraser import cloud_architecture as diagram
 
 
 class Repository(ecr.Repository):
-    def __init__(self, name: str, **kwargs) -> None:
+    def __init__(
+        self,
+        resource_name: str,
+        name: Optional[str] = None,
+        **kwargs,
+    ) -> None:
         """Create a new ECR Repository.
 
         Args:
-            name (str): The name of the ECR Repository.
+            resource_name (str): The name of this resource.
+            name (Optional[str]): Name of the repository. Defaults to None.
             **kwargs: [additional arguments](https://www.pulumi.com/registry/packages/aws/api-docs/ecr/repository/#inputs)
         """
-        super().__init__(name, **kwargs)
-        self.diagram = diagram.Node(name, icon="aws-elastic-container-registry")
+        super().__init__(resource_name, name=name, **kwargs)
+        self.diagram = diagram.Node(
+            resource_name, icon="aws-elastic-container-registry"
+        )
