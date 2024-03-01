737717882e8c48a986b2e9ce7962c7d52a94243b
wenke.lai <wenke.studio@gmail.com>
2024-03-01 14:48:43 +0800

    fix(aws.cloudfront): update the import path fo resources

--- integrator/aws/cloudfront/__init__.py
+++ integrator/aws/cloudfront/__init__.py
@@ -1,5 +1,11 @@
+from .cache_policy import CachePolicy
 from .distribution import Distribution
+from .origin_access_control import OriginAccessControl
+from .response_header_policy import ResponseHeaderPolicy
 
 __all__ = [
+    "CachePolicy",
     "Distribution",
+    "OriginAccessControl",
+    "ResponseHeaderPolicy",
 ]
