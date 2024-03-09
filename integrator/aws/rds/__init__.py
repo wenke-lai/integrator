8bc83a66d83462c404a2c3da36dffbed62ccd5bd
wenke.lai <wenke.studio@gmail.com>
2024-03-09 23:53:04 +0800

    fix(aws): add import for aurora-mysql serverless v2 to rds

--- integrator/aws/rds/__init__.py
+++ integrator/aws/rds/__init__.py
@@ -1,7 +1,8 @@
-from .cluster import Cluster
+from .cluster import AuroraMySQLServerlessV2, Cluster
 from .subnet_group import SubnetGroup
 
 __all__ = [
+    "AuroraMySQLServerlessV2",
     "Cluster",
     "SubnetGroup",
 ]
